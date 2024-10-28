1b882c0e9ac5f3a2b5a7fd7af7d2d2c6abfd7fb1
westjin <uko802@naver.com>
2024-10-28 20:49:44 +0900

    [FEAT] 프로필 서비스 구현

--- src/main/java/uni/backend/service/ProfileService.java
+++ src/main/java/uni/backend/service/ProfileService.java
@@ -3,11 +3,13 @@ package uni.backend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uni.backend.domain.MainCategory;
 import uni.backend.domain.Profile;
 import uni.backend.repository.ProfileRepository;
 import uni.backend.domain.User;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -24,18 +26,21 @@ public class ProfileService {
 
 
     //    사용자 ID를 통해 Profile을 찾는 메서드
-    public Optional<Profile> findProfileByUserId(Long userId) {
-        return profileRepository.findByUserId(userId);
+    public Optional<Profile> findProfileByUserId(Integer userId) {
+        return profileRepository.findByUser_UserId(userId);
     }
 
+
+
+    // 프로필 업데이트
     @Transactional
-    public Profile updateProfile(Long userId, String content, String hashtag) {
-        Profile profile = profileRepository.findByUserId(userId)
+    public Profile updateProfile(Integer userId, String content, List<MainCategory> mainCategories) {
+        Profile profile = profileRepository.findByUser_UserId(userId)
                 .orElseThrow(() -> new IllegalArgumentException("프로필이 존재하지 않습니다. : " + userId));
 
         profile.setContent(content);
-        profile.setHashtag(hashtag);
         profile.setUpdatedAt(LocalDateTime.now());
+        profile.setMainCategories(mainCategories);
 
         return profileRepository.save(profile);
     }
