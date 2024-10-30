ba26edc6363ca8f5d4a3b6fa16a69989cefa327b
westjin <uko802@naver.com>
2024-10-30 23:25:42 +0900

    [FEAT] MainCategoryDTO 생성

---
+++ src/main/java/uni/backend/domain/dto/MainCategoryDTO.java
@@ -0,0 +1,12 @@
+package uni.backend.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainCategoryDTO {
+    private Integer mainCategoryId; // 카테고리 ID
+    private Integer hashtagId; // 해시태그 ID
+}
+
