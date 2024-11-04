29a03feb0c4e623ec5028c98a771aee2e0fb861a
westjin <uko802@naver.com>
2024-11-04 15:28:49 +0900

    [FEAT] 대학교 도메인 추가

---
+++ src/main/java/uni/backend/domain/University.java
@@ -0,0 +1,25 @@
+package uni.backend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class University {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "university_id")
+  private Integer universityId;
+
+  @Column(nullable = false, length = 255)
+  private String uniName;
+
+
+}
