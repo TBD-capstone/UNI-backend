6bc598ecfefd903d7165c1e6e89769f32ce57fba
westjin <uko802@naver.com>
2024-11-09 01:08:29 +0900

    [FEAT] Reply 작성 요청 리스폰스 바디를 위한 dto 생성

---
+++ src/main/java/uni/backend/domain/dto/ReplyCreateResponse.java
@@ -0,0 +1,17 @@
+package uni.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReplyCreateResponse {
+
+  private String status;
+  private String message;
+  private ReplyResponse reply; // 대댓글 정보
+
+  public static ReplyCreateResponse success(String message, ReplyResponse reply) {
+    return new ReplyCreateResponse("success", message, reply);
+  }
+}
