d7054d5f5d30e586c418c1aabbb8edc0b51d9e68
westjin <uko802@naver.com>
2024-11-14 15:25:28 +0900

    [FEAT] 마커 DTO 추가

---
+++ src/main/java/uni/backend/domain/dto/MarkerResponse.java
@@ -0,0 +1,23 @@
+package uni.backend.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MarkerResponse {
+
+    private Integer id;
+    private double latitude;
+    private double longitude;
+    private String name;
+    private String description;
+
+}
