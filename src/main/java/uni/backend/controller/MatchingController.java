5d56f09625eea2defbfc693f97b8389dbd9eee9e
jaeyong <ljy9085@ajou.ac.kr>
2024-11-15 23:52:44 +0900

    [REFACTOR] MatchingController에서 requestId를 matchingId로 변경 및 JSON 응답 포맷 수정

--- src/main/java/uni/backend/controller/MatchingController.java
+++ src/main/java/uni/backend/controller/MatchingController.java
@@ -1,6 +1,8 @@
 package uni.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -29,7 +31,8 @@ public class MatchingController {
     private SimpMessagingTemplate messagingTemplate;
 
     @PostMapping("/request")
-    public MatchingCreateResponse createMatchRequest(@RequestBody MatchingCreateRequest matchingCreateRequest) {
+    public ResponseEntity<MatchingCreateResponse> createMatchRequest(@RequestBody MatchingCreateRequest matchingCreateRequest) {
+
         User requester = userService.findById(matchingCreateRequest.getRequesterId());
         User receiver = userService.findById(matchingCreateRequest.getReceiverId());
 
@@ -41,24 +44,33 @@ public class MatchingController {
 
         Matching savedRequest = matchingService.createRequest(request, requester, receiver);
 
+        MatchingCreateResponse response = MatchingCreateResponse.from(savedRequest);
+
         messagingTemplate.convertAndSend("/sub/match-request/" + matchingCreateRequest.getReceiverId(), "매칭 요청이 도착했습니다.");
 
-        return MatchingCreateResponse.from(savedRequest);
+        return ResponseEntity.ok(response);
     }
 
+
     @PostMapping("/respond")
-    public MatchingUpdateResponse respondToMatchRequest(@RequestBody MatchingUpdateRequest matchingUpdateRequest) {
+    public ResponseEntity<String> respondToMatchRequest(@RequestBody MatchingUpdateRequest matchingUpdateRequest) {
         Optional<Matching> requestOpt = matchingService.updateRequestStatus(matchingUpdateRequest);
 
         if (requestOpt.isPresent()) {
             Matching request = requestOpt.get();
-            String responseMessage = matchingUpdateRequest.isAccepted() ? "매칭 요청이 수락되었습니다." : "매칭 요청이 거절되었습니다.";
+
+            String responseMessage = matchingUpdateRequest.isAccepted()
+                    ? "매칭 요청이 수락되었습니다."
+                    : "매칭 요청이 거절되었습니다.";
+
             messagingTemplate.convertAndSend("/sub/match-response/" + request.getRequester().getUserId(), responseMessage);
 
-            return MatchingUpdateResponse.from(request);
+            return ResponseEntity.ok("응답 처리 완료");
         }
-        throw new IllegalArgumentException("요청을 찾을 수 없습니다.");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("요청을 찾을 수 없습니다.");
     }
+
 
     @MessageMapping("/match/request")
     @SendTo("/sub/match/request")
