3958654a111eafb17ebadf181ec57420c3ac66b7
jaeyong <ljy9085@ajou.ac.kr>
2024-11-15 23:53:18 +0900

    [REFACTOR] MatchingCreateResponse에서 requestId를 matchingId로 변경

--- src/main/java/uni/backend/domain/dto/MatchingCreateResponse.java
+++ src/main/java/uni/backend/domain/dto/MatchingCreateResponse.java
@@ -3,19 +3,27 @@ package uni.backend.domain.dto;
 import lombok.*;
 import uni.backend.domain.Matching;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MatchingCreateResponse {
-    private Integer requestId;
+    private Integer matchingId;
+    private Integer requesterId;
+    private Integer receiverId;
     private String status;
+    private LocalDateTime createdAt;
 
     public static MatchingCreateResponse from(Matching matching) {
         return MatchingCreateResponse.builder()
-                .requestId(matching.getRequestId())
+                .matchingId(matching.getMatchingId())
+                .requesterId(matching.getRequester().getUserId())
+                .receiverId(matching.getReceiver().getUserId())
                 .status(matching.getStatus().name())
+                .createdAt(matching.getCreatedAt())
                 .build();
     }
 }
