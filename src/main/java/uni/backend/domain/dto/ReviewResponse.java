9dbb21af6cfe9703ee62bcde99265efa14f6d5c3
westjin <uko802@naver.com>
2024-11-16 23:08:00 +0900

    [FEAT] 리뷰 DTO 생성

---
+++ src/main/java/uni/backend/domain/dto/ReviewResponse.java
@@ -0,0 +1,28 @@
+package uni.backend.domain.dto;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ReviewResponse {
+
+    private Integer reviewId;
+    private Integer profileOwnerId;
+    private String profileOwnerName;
+    private Integer commenterId;
+    private String commenterName;
+    private String commenterImgProf; // 작성자의 프로필 이미지 추가
+    private String content;
+    private Integer star;
+    private Long likes;
+    private Boolean deleted;
+    private LocalDateTime deletedTime;
+    private LocalDateTime updatedTime;
+    private String deleteMessage;
+    private List<ReviewReplyResponse> replies;
+
+}
