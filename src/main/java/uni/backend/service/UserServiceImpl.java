4c6464b9696744f451b580c8383451350ab0a461
jaeyong <ljy9085@ajou.ac.kr>
2024-11-25 02:26:40 +0900

    [HOTFIX] 작동 안하는 부분 삭제

--- src/main/java/uni/backend/service/UserServiceImpl.java
+++ src/main/java/uni/backend/service/UserServiceImpl.java
@@ -78,7 +78,6 @@ public class UserServiceImpl implements UserService {
         resetCodes.put(email, code);
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("UNI");
         message.setTo(email);
         message.setSubject("Password Reset Code");
         message.setText("Your password reset code is: " + code);
