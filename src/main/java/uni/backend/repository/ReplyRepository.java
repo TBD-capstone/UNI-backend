b07b4fe3145ea7108c0d5cac07bcd458492f2550
westjin <uko802@naver.com>
2024-11-25 00:42:42 +0900

    [FEAT] 유저 제재에 따른 작성글 블라인드 적용 기능 추가

--- src/main/java/uni/backend/repository/ReplyRepository.java
+++ src/main/java/uni/backend/repository/ReplyRepository.java
@@ -1,5 +1,6 @@
 package uni.backend.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uni.backend.domain.Qna;
@@ -7,5 +8,8 @@ import uni.backend.domain.Reply;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Integer> {
+
+    List<Reply> findByCommenter_UserId(Integer userId); // 작성자 ID로 답글 조회
+
 
 }
