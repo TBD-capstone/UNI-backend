18c611385b2aa9198aed745a64d2f9bb1c739519
Sunwoo Song <gbe0808@gmail.com>
2024-11-27 02:12:37 +0900

    [TEST] QnA 번역 JUnit 테스트 완료

--- src/main/java/uni/backend/service/PageTranslationService.java
+++ src/main/java/uni/backend/service/PageTranslationService.java
@@ -3,17 +3,21 @@ package uni.backend.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uni.backend.domain.dto.HomeDataResponse;
 import uni.backend.domain.dto.HomeProfileResponse;
 import uni.backend.domain.dto.IndividualProfileResponse;
 import uni.backend.domain.dto.IndividualTranslationResponse;
+import uni.backend.domain.dto.QnaResponse;
+import uni.backend.domain.dto.ReplyResponse;
 import uni.backend.domain.dto.TranslationRequest;
 import uni.backend.domain.dto.TranslationResponse;
 
 @Service
 public class PageTranslationService {
 
+    @Autowired
     private TranslationService translationService;
 
     private List<String> extractUnivHashtag(String univName, List<String> hashtags,
@@ -52,7 +56,7 @@ public class PageTranslationService {
         }
     }
 
-    private void translateHomeDataResponse(HomeDataResponse homeDataResponse,
+    public void translateHomeDataResponse(HomeDataResponse homeDataResponse,
         String acceptLanguage) {
         if (homeDataResponse == null || homeDataResponse.getData() == null) {
             return;
@@ -64,6 +68,29 @@ public class PageTranslationService {
             profile.setUnivName(data.getFirst());
             if (data.size() > 1) {
                 profile.setHashtags(data.subList(1, data.size()));
+            }
+        }
+    }
+
+    public void translateQna(List<QnaResponse> response, String acceptLanguage) {
+        TranslationRequest translationRequest = new TranslationRequest();
+        translationRequest.setTarget_lang(acceptLanguage);
+
+        for (QnaResponse individualQnaResponse : response) {
+            List<String> text = new ArrayList<>(List.of());
+            text.add(individualQnaResponse.getContent());
+            for (ReplyResponse individualReplyResponse : individualQnaResponse.getReplies()) {
+                text.add(individualReplyResponse.getContent());
+            }
+            translationRequest.setText(text);
+            TranslationResponse translationResponse = translationService.translate(
+                translationRequest);
+            List<IndividualTranslationResponse> translations = translationResponse.getTranslations();
+            individualQnaResponse.setContent(translations.getFirst().getText());
+
+            int i = 1;
+            for (ReplyResponse individualReplyResponse : individualQnaResponse.getReplies()) {
+                individualReplyResponse.setContent(translations.get(i++).getText());
             }
         }
     }
