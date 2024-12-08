baa8100d8d9265700be09edf1cf64727f1d26db1
jaeyong <ljy9085@ajou.ac.kr>
2024-12-09 01:18:18 +0900

    [FIX] Builder 패턴 추가

--- src/main/java/uni/backend/domain/dto/ReviewCreateRequest.java
+++ src/main/java/uni/backend/domain/dto/ReviewCreateRequest.java
@@ -3,9 +3,11 @@ package uni.backend.domain.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ReviewCreateRequest {
 
 
