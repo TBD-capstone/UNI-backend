8da5f4d01a8c0bf9a81620d521b231796af27f63
jaeyong <ljy9085@ajou.ac.kr>
2024-12-09 01:41:10 +0900

    [FIX] Builder 패턴 추가

--- src/main/java/uni/backend/domain/dto/ReviewReplyCreateRequest.java
+++ src/main/java/uni/backend/domain/dto/ReviewReplyCreateRequest.java
@@ -1,8 +1,10 @@
 package uni.backend.domain.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ReviewReplyCreateRequest {
 
     private String content; // 단순히 content 필드만 처리
