fea9372df43fb566c48247e16f5810b887641bf8
jaeyong <ljy9085@ajou.ac.kr>
2024-12-09 00:56:56 +0900

    [FIX] Builder 패턴 추가

--- src/main/java/uni/backend/domain/dto/ReplyResponse.java
+++ src/main/java/uni/backend/domain/dto/ReplyResponse.java
@@ -1,11 +1,13 @@
 package uni.backend.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 public class ReplyResponse {
 
